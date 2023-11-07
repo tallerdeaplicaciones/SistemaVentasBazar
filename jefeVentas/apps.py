1515d301a32f6d9babed49c5526cfa32d4874308
BastianPerezG <bastianesteban.perez@gmail.com>
2023-11-07 18:15:03 -0300

    arreglando jeveVentas

--- jefeVentas/apps.py
+++ jefeVentas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JeveventasConfig(AppConfig):
+class JefeventasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jeveVentas'
+    name = 'jefeVentas'
