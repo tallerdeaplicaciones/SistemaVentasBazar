ddd3908167eaa5bf2746e821853837a7448792f2
JseMatamoros <josematamoros916@gmail.com>
2023-11-12 22:14:17 -0300

    agregando registration

--- jefeVentas/forms.py
+++ jefeVentas/forms.py
@@ -2,9 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
-    tipo_usuario = forms.ChoiceField(choices=[('jefe_ventas', 'Jefe de Ventas'), ('vendedor', 'Vendedor')])
-
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = UserCreationForm.Meta.fields + ('tipo_usuario',)
+        model = Profile
+        fields = ['name', 'last_name', 'user']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+            'user': forms.Select(attrs={'class':'form-control'})
+        }
