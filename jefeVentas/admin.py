847238c6f165766b2a09a26bd76b296523264258
BastianPerezG <bastianesteban.perez@gmail.com>
2023-11-13 22:41:58 -0300

    agregando tabla productos-categoria y datos prueba

--- jefeVentas/admin.py
+++ jefeVentas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Categoria, Productos
 # Register your models here.
+
+
+admin.site.register(Categoria)
+
+admin.site.register(Productos)
--- jefeVentas/models.py
+++ jefeVentas/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    precio = models.IntegerField(null=False, blank=False)
+    descripcion = models.TextField(null=False, blank=False)
+    stock = models.IntegerField(null=False, blank=False)
+    imagen = models.ImageField(upload_to='productos',null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
