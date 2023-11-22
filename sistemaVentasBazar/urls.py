5badbcd8031eabb1016cc31bda0ae633121b9344
JseMatamoros <josematamoros916@gmail.com>
2023-11-22 14:22:00 -0300

    solo comente una linea para que no moleste los errores

--- sistemaVentasBazar/urls.py
+++ sistemaVentasBazar/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),  
     path('vendedor/', include('vendedor.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
